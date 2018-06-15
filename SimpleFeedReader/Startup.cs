9d018b9768a8feb14462bf8a819a0b1f9aea84e3
Scott Addie <scott.addie@microsoft.com>
2018-06-15 15:43:01 -0500

    Upgrade app to 2.1

--- SimpleFeedReader/Startup.cs
+++ SimpleFeedReader/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleFeedReader.Repositories;
@@ -19,7 +20,7 @@ namespace SimpleFeedReader
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<NewsItemRepository>();
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +28,6 @@ namespace SimpleFeedReader
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
